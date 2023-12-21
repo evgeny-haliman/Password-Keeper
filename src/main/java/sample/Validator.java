feff3fbcbd6ce75f5148fb6ce4882cef0263bcd8
Evgeny <eugen.haliman@gmail.com>
2023-12-21 23:39:37 +0200

    changed regex

--- src/main/java/sample/Validator.java
+++ src/main/java/sample/Validator.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public class Validator {
 
     public boolean isValidPassword(String password) {
-        String patternString = "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{16,}";
+        String patternString = "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()~{}])(?=\\S+$).{16,}";
         return Pattern.matches(patternString, password);
     }
 }
